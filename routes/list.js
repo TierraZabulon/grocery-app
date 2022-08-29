9082038bb7c35d930a76e5c53ce40a1f5c8a4838
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-08-29 18:42:24 -0400

    updated routes

--- routes/list.js
+++ routes/list.js
@@ -1,3 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const listController = require("../controllers/list");
+// const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
+// router.get('/', ensureAuth, todosController.getTodos)
+
+router.post("/createlist", listController.createlist);
+
+router.put("/markComplete", listController.markComplete);
+
+router.put("/markIncomplete", listController.markIncomplete);
+
+router.delete("/deletelist", listController.deletelist);
+
+module.exports = router;
